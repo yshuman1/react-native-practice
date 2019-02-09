183b7ffe1efb7535efe3fca618b01ac9c093700f
Yasin Shuman <email@yasin.io>
2019-02-08 21:19:23 -0800

    styling shizzzzzz

--- src/screens/Auth/Auth.js
+++ src/screens/Auth/Auth.js
@@ -1,21 +1,65 @@
-import React, { Component } from 'react';
-import { View, Text, Button } from 'react-native';
+import React, { Component } from 'react'
+import {
+  View,
+  Text,
+  Button,
+  TextInput,
+  StyleSheet,
+  ImageBackground
+} from 'react-native'
 
-import startMainTabs from '../MainTabs/startMainTabs';
+import startMainTabs from '../MainTabs/startMainTabs'
+import DefaultInput from '../../components/UI/DefaultInput/DefaultInput'
+import HeadingText from '../../components/UI/HeadingText/HeadingText'
+import MainText from '../../components/UI/MainText/MainText'
+import ButtonWithBackground from '../../components/UI/ButtonWithBackground/ButtonWithBackground'
+
+import backgroundImage from '../../assets/background.jpg'
 
 class AuthScreen extends Component {
-    loginHandler = () => {
-        startMainTabs();
-    }
+  loginHandler = () => {
+    startMainTabs()
+  }
 
-    render () {
-        return (
-            <View>
-                <Text>Auth Screen</Text>
-                <Button title="Login" onPress={this.loginHandler}/>
-            </View>
-        );
-    }
+  render() {
+    return (
+      <ImageBackground style={styles.backgroundImage} source={backgroundImage}>
+        <View style={styles.container}>
+          <MainText>
+            <HeadingText>Please Log In</HeadingText>
+          </MainText>
+          <ButtonWithBackground
+            onPress={() => alert('hey there!')}
+            color='#29aaf4'
+          >
+            Switch to Login
+          </ButtonWithBackground>
+          <View style={styles.inputContainer}>
+            <DefaultInput
+              placeholder='your email address'
+              style={styles.input}
+            />
+            <DefaultInput placeholder='password' style={styles.input} />
+            <DefaultInput placeholder='confirm password' style={styles.input} />
+          </View>
+          <ButtonWithBackground onPress={this.loginHandler} color='#29aaf4'>
+            Submit
+          </ButtonWithBackground>
+        </View>
+      </ImageBackground>
+    )
+  }
 }
 
-export default AuthScreen;
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  backgroundImage: { width: '100%', flex: 1 },
+  input: { backgroundColor: '#eee', borderColor: '#bbb' },
+  inputContainer: { width: '80%' }
+})
+
+export default AuthScreen
--- src/screens/SharePlace/SharePlace.js
+++ src/screens/SharePlace/SharePlace.js
@@ -1,43 +1,103 @@
-import React, { Component } from 'react';
-import { View, Text } from 'react-native';
-import { connect } from 'react-redux';
+import React, { Component } from 'react'
+import {
+  View,
+  Text,
+  TextInput,
+  Button,
+  StyleSheet,
+  ScrollView,
+  Image
+} from 'react-native'
+import { connect } from 'react-redux'
 
-import PlaceInput from '../../components/PlaceInput/PlaceInput';
-import { addPlace } from '../../store/actions/index';
+import { addPlace } from '../../store/actions/index'
+import PlaceInput from '../../components/PlaceInput/PlaceInput'
+import MainText from '../../components/UI/MainText/MainText'
+import HeadingText from '../../components/UI/HeadingText/HeadingText'
+import PickImage from '../../components/PickImage/PickImage'
+import PickLocation from '../../components/PickLocation/PickLocation'
 
 class SharePlaceScreen extends Component {
-    constructor(props) {
-        super(props);
-        this.props.navigator.setOnNavigatorEvent(this.onNavigatorEvent);
-    }
+  state = {
+    placeName: ''
+  }
+  constructor(props) {
+    super(props)
+    this.props.navigator.setOnNavigatorEvent(this.onNavigatorEvent)
+  }
 
-    onNavigatorEvent = event => {
-        if (event.type === "NavBarButtonPress") {
-            if (event.id === "sideDrawerToggle") {
-                this.props.navigator.toggleDrawer({
-                    side: "left"
-                });
-            } 
-        }  
+  onNavigatorEvent = event => {
+    if (event.type === 'NavBarButtonPress') {
+      if (event.id === 'sideDrawerToggle') {
+        this.props.navigator.toggleDrawer({
+          side: 'left'
+        })
+      }
     }
+  }
 
-    placeAddedHandler = placeName => {
-        this.props.onAddPlace(placeName);
-    }
+  placeNameChangedHandler = val => {
+    this.setState({
+      placeName: val
+    })
+  }
 
-    render () {
-        return (
-            <View>
-                <PlaceInput onPlaceAdded={this.placeAddedHandler}/>
-            </View>
-        );
+  placeAddedHandler = () => {
+    if (this.state.placeName.toString() !== '') {
+      this.props.onAddPlace(this.state.placeName)
     }
+  }
+
+  render() {
+    return (
+      <ScrollView>
+        <View style={styles.container}>
+          <MainText>
+            <HeadingText>Share a Place with us!</HeadingText>
+          </MainText>
+          <PickImage />
+          <PickLocation />
+          <PlaceInput
+            placeName={this.state.placeName}
+            onChangeText={this.placeNameChangedHandler}
+          />
+          <View style={styles.button}>
+            <Button title='Share the Place!' onPress={this.placeAddedHandler} />
+          </View>
+        </View>
+      </ScrollView>
+    )
+  }
 }
 
-const mapDispatchToProps = dispatch => {
-    return {
-        onAddPlace: (placeName) => dispatch(addPlace(placeName))
-    };
-};
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center'
+  },
+  placeholder: {
+    borderWidth: 1,
+    borderColor: 'black',
+    backgroundColor: '#eee',
+    width: '80%',
+    height: 150
+  },
+  button: {
+    margin: 8
+  },
+  previewImage: {
+    width: '100%',
+    height: '100%'
+  }
+})
 
-export default connect(null, mapDispatchToProps)(SharePlaceScreen);
+const mapDispatchToProps = dispatch => {
+  return {
+    onAddPlace: placeName => dispatch(addPlace(placeName))
+  }
+}
+
+export default connect(
+  null,
+  mapDispatchToProps
+)(SharePlaceScreen)
